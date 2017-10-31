18c9987fcc18cdc645a47adc3e9db081e8de5b50
SixQuant <caviler@gmail.com>
2017-10-31 23:09:39 +0800

    first commit

--- demo/demo.py
+++ demo/demo.py
@@ -8,5 +8,6 @@ watermark_template_filename = path + 'anjuke-watermark-template.jpg'
 remover = WatermarkRemover()
 remover.load_watermark_template(watermark_template_filename)
 
+remover.remove_watermark(path + 'anjuke2.jpg', path + 'anjuke2-result.jpg')
 remover.remove_watermark(path + 'anjuke3.jpg', path + 'anjuke3-result.jpg')
 remover.remove_watermark(path + 'anjuke4.jpg', path + 'anjuke4-result.jpg')
