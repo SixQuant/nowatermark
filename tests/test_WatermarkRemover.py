1e2fa2799e84d24bd1d52f6ca2e14e68681ff7f7
SixQuant <caviler@gmail.com>
2017-11-01 12:36:24 +0800

    Travis CI Install OpenCV

--- tests/test_WatermarkRemover.py
+++ tests/test_WatermarkRemover.py
@@ -4,15 +4,21 @@ import os
 import sys
 import unittest
 
-sys.path.append(os.path.abspath(os.path.expanduser(__file__ + '/../..')))
+root = os.path.abspath(os.path.expanduser(__file__ + '/../..'))
+sys.path.append(root)
 
 import nowatermark
 
 
 class TestMethods(unittest.TestCase):
-    def test_add(self):
+    def test_all(self):
         remover = nowatermark.WatermarkRemover()
-        self.assertIsNotNone(remover)
+
+        path = root + '/data/'
+        watermark_template_filename = path + 'anjuke-watermark-template.jpg'
+        remover.load_watermark_template(watermark_template_filename)
+
+        self.assertIsNotNone(remover.remove_watermark(path + 'anjuke2.jpg', path + 'anjuke2-result.jpg'))
 
 
 if __name__ == '__main__':
